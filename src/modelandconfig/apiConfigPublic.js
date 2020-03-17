7aee45daec86331f7c1e54f0538ea39f61da4e61
majacobs <majacobs@kth.se>
2020-03-17 17:10:59 +0100

    Revert "test-commit"
    This reverts commit 3713efbdca77fc0a42f5712f1cf307564905d9a7.

--- src/modelandconfig/apiConfigPublic.js
+++
@@ -1,4 +0,0 @@
-const ClientIdSpotify = "6b81aee6295f4241b1442fd768e26a0e";
-const FireBaseCollectionID = "uq5qdkdYhTG3mbdaJtiD";
-const heroApiENDPOINT = "https://superheroapi.com/api/"
-const HeroApiAccessToken = "10216578763606325/";
