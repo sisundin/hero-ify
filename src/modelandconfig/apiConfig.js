504b8ac6bfa52dc3b78978cc12fd625592b2922b
sisundin <simonsundinsmail@gmail.com>
2020-03-19 13:08:09 +0100

    hemliga grejor

--- src/modelandconfig/apiConfig.js
+++ src/modelandconfig/apiConfig.js
@@ -2,3 +2,11 @@ const ClientIdSpotify = "6b81aee6295f4241b1442fd768e26a0e";
 const FireBaseCollectionID = "uq5qdkdYhTG3mbdaJtiD";
 const heroApiENDPOINT = "https://superheroapi.com/api/"
 const HeroApiAccessToken = "10216578763606325/";
+var admin = require("firebase-admin");
+
+var serviceAccount = require("src/modelandconfig/hero-ify-firebase-adminsdk-k8ano-b2c3cd553c.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://hero-ify.firebaseio.com"
+});
