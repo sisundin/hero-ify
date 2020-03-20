d706048966bce0edcfe72f6f9c265b0ddf0cbf4b
majacobs <majacobs@kth.se>
2020-03-20 11:38:47 +0100

    liten test

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import React from 'react';
-import './App.css';
-import HeroIfyModel from './modelconfig/model.js';
-import signInView from '.sigin/signin.js'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-
-} from "react-router-dom"
-
+import React from "react";
+import "./App.css";
+import HeroIfyModel from "./modelandconfig/model.js";
+import signInView from "./signin/signInView.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {
-    }
-  } 
+    super(props);
+    this.state = {};
+  }
 
-
-  render(){
+  render() {
     return (
       <Router>
-          <Switch>
-            <React.Fragment></React.Fragment>
-              <div className = "main">
-                <div className = "Welcome debug">
-                  <Route path="/" render={() => <signInView model={HeroIfyModel}/>}/>
-              </div>
+        <Switch>
+          <React.Fragment></React.Fragment>
+          <div className="main">
+            <div className="Welcome debug">
+              <Route
+                path="/"
+                render={() => <signInView model={HeroIfyModel} />}
+              />
             </div>
-          </Switch>
+          </div>
+        </Switch>
       </Router>
     );
- 
   }
 }
 
 export default App;
 
 //<div className = "mainContent hide debug" id="details">
-  //      <DishDetailsContainer model = {dinnerModel}/>
-    //    </div>
+//      <DishDetailsContainer model = {dinnerModel}/>
+//    </div>
