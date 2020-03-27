3080c8d23ce6e90ffe1b6a9fcf721cf988db5222
trooligt <45091189+trooligt@users.noreply.github.com>
2020-03-27 12:49:32 +0100

    choosehero updated

--- src/choosehero/chooseHero.js
+++ src/choosehero/chooseHero.js
@@ -49,7 +49,7 @@ export default class ChooseHero extends React.Component{
          h("div", {}, 
             h("div",{className:"divider"},null ),
             h("div", {className:"searchbox"},
-            <input id="searchInput" />,   // free text search box
+            <input id="searchInput" placeholder="eg Batman" />,   // free text search box
             <Button variant="btn btn-success" onClick ={ () => this.update()}>Search!</Button>
             ),
             h("div",{className:"divider"},null ),
@@ -61,12 +61,6 @@ export default class ChooseHero extends React.Component{
             />
             
         ),
-        <div className="divider"></div>,
-        <p>Chosen hero: {this.props.model.getHeroName()}</p>,
-        h("div",{className:"divider"},null ),
-        <div class="text-center">
-            <this.RenderNextButton argument = {this.props.model.getHeroName()}/>
-        </div>
         )))
     }
     
@@ -74,12 +68,11 @@ export default class ChooseHero extends React.Component{
       return <div id={hero.id}>
       <img className = "heroPic" src={hero.images.lg} alt="img"></img>
       <div className="divider"></div>
-      <p>{hero.name}</p>
       <div className="divider"></div>
       <div class="text-center">
-      <Button variant="btn btn-success btn-lg" onClick={() => {
-          this.props.model.setHero(hero)
-        }}>Choose</Button>
+      <a href="/specPlaylist"> <Button variant="btn btn-success btn-lg" onClick={() => {
+          this.props.model.setHero(hero) 
+        }}>Select {hero.name}</Button></a>
         </div>
         </div>    
     }
