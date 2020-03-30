5b7262c5e745e0483a65811bddca45d2bc49e202
nikeer <nikeer@kth.se>
2020-03-30 21:41:13 +0200

    Updates

---
+++ precache-manifest.08adcc7d2fbbd7ab4138baa1488b431c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14e2b8f3bbfafdf24a54cf48e799c47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c273c9e6ebe867759d",
+    "url": "/static/css/2.106cde16.chunk.css"
+  },
+  {
+    "revision": "7430e39f8895878f0bb8",
+    "url": "/static/css/main.c538de19.chunk.css"
+  },
+  {
+    "revision": "d9c273c9e6ebe867759d",
+    "url": "/static/js/2.cb3bf5e9.chunk.js"
+  },
+  {
+    "revision": "3272418a5f03796eea40d8ddb7225b90",
+    "url": "/static/js/2.cb3bf5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7430e39f8895878f0bb8",
+    "url": "/static/js/main.531b4b85.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "26bc35b5aead84f29098c9c6401de5b5",
+    "url": "/static/media/wave.26bc35b5.svg"
+  }
+]);
