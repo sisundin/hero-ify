56b3baa66a2ddaf15fee8ce50a7b40246c662f88
majacobs <majacobs@kth.se>
2020-04-19 11:49:45 +0200

    start playlistcreator

--- src/modelandconfig/model.js
+++ src/modelandconfig/model.js
@@ -11,14 +11,14 @@ class HeroIfyModel extends React.Component {
     this.subscribers = [];
     this.hero = {
       name: "You need to pick a hero!",
-      images: { lg: "no image" }
+      images: { lg: "no image" },
     };
     this.playlistAttributes = {
       userID: "",
       genres: [],
       mood: "",
       energy: "",
-      length: ""
+      length: "",
     };
     firebase.initializeApp(firebaseConfig);
     this.db = firebase.database();
@@ -27,7 +27,7 @@ class HeroIfyModel extends React.Component {
       spotifyApi.setAccessToken(params.access_token);
     }
     this.state = {
-      loggedIn: params.access_token ? true : false
+      loggedIn: params.access_token ? true : false,
     };
   }
 
@@ -36,40 +36,26 @@ class HeroIfyModel extends React.Component {
   }
 
   removeObserver(callback) {
-    callback = this.subscribers.filter(o => o !== callback);
-    };
+    callback = this.subscribers.filter((o) => o !== callback);
+  }
 
-    notifyObservers(whatHappened){
-        this.subscribers.forEach(function(callback){
-            callback(whatHappened);
-       });
-    }
-
-
-    getHeroData(string) {
-        return fetch("https://superhero-search.p.rapidapi.com/?"+string, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": heroApihost,
-                "x-rapidapi-key": HeroApiAccessKey
-            }
-        }).then(response => this.handleHTTPError(response))
-        .then(response => response.json())
-        .catch(error => console.log(error));
-
-    }
+  notifyObservers(whatHappened) {
+    this.subscribers.forEach(function (callback) {
+      callback(whatHappened);
+    });
+  }
 
   getHeroData(string) {
     return fetch("https://superhero-search.p.rapidapi.com/?" + string, {
       method: "GET",
       headers: {
         "x-rapidapi-host": heroApihost,
-        "x-rapidapi-key": HeroApiAccessKey
-      }
+        "x-rapidapi-key": HeroApiAccessKey,
+      },
     })
-      .then(response => this.handleHTTPError(response))
-      .then(response => response.json())
-      .catch(error => console.log(error));
+      .then((response) => this.handleHTTPError(response))
+      .then((response) => response.json())
+      .catch((error) => console.log(error));
 
     ///const http = new XMLHttpRequest();
     ///http.open("GET", heroApiENDPOINT+HeroApiAccessToken+string);
@@ -143,14 +129,14 @@ class HeroIfyModel extends React.Component {
       powerstats.durability +
       powerstats.combat;
     let genres = {
-      "classical": powerstats.intelligence / allstats,
-      "punk": powerstats.strength / allstats,
-      "pop": powerstats.speed / allstats,
-      "lowfy beats": powerstats.durability / allstats,
+      classical: powerstats.intelligence / allstats,
+      punk: powerstats.strength / allstats,
+      pop: powerstats.speed / allstats,
+      "lo fi beats": powerstats.durability / allstats,
       "electronic dance": powerstats.power / allstats,
-      "hip hop": powerstats.combat / allstats
+      "hip hop": powerstats.combat / allstats,
     };
-    return genres;
+    genres.forEach((genre) => this.playlistAttributes.genres.push(genre));
   }
 
   //getPlaylists NEEDS RENDER PROMIS
@@ -160,15 +146,15 @@ class HeroIfyModel extends React.Component {
       .ref("UserGenereatedPlaylists")
       .limitToLast(limit)
       .once("value")
-      .then(snapshot => {
+      .then((snapshot) => {
         snapshot = snapshot.toJSON();
         Object.values(snapshot)
           .reverse()
-          .forEach(doc => {
+          .forEach((doc) => {
             playlists.push({
               Hero: doc.Hero,
               PlaylistLink: doc.PlaylistLink,
-              User: doc.User
+              User: doc.User,
             });
           });
         return playlists;
@@ -180,25 +166,37 @@ class HeroIfyModel extends React.Component {
     this.db.ref("UserGenereatedPlaylists/" + user).set({
       Hero: heroname,
       PlaylistLink: playlistlink,
-      User: user
+      User: user,
     });
   }
 
-  //spotify playlist function
-  spotifyApiConnect() {}
-
   generatePlaylistId() {
-    //var playlistID = spotifyApi.createPlaylist(this.playlistAttributes.userID)
-    //return playlistID
+    var playlistID = spotifyApi.createPlaylist(this.playlistAttributes.userID);
+    return playlistID;
   }
 
   generateSpotifyPlaylist() {
-    //put together the playlist based on user input
+    var userId = this.playlistAttributes.userId;
+    spotifyApi.createPlaylist(userId, options);
   }
 
-  addTrackToPlaylist() {
-    //add tracks to playlist
-    //var Spotify.add
+  getGenreShare(genre_ratio, genre) {
+    var genreShare = [];
+    const mood = this.playlistAttributes.mood;
+    const energy = this.playlistAttributes.energy;
+    const length = this.playlistAttributes.length;
+    const attributes = {
+      target_valence: mood,
+      target_energy: energy,
+      limit: genre_ratio * length,
+      seed_genres: { genre },
+    };
+
+    spotifyApi.getRecommendations(attributes).then((response) => {
+      for (var i = 0, l = response.items.length; i < l; i++) {
+        genreShare.push(response.items[i]);
+      }
+    });
   }
 
   getHashParams() {
@@ -216,7 +214,7 @@ class HeroIfyModel extends React.Component {
 
   getMyTopTracks() {
     var alltrackstoptracks = [];
-    spotifyApi.getMyTopTracks({ limit: 100 }).then(response => {
+    spotifyApi.getMyTopTracks({ limit: 100 }).then((response) => {
       for (var i = 0, l = response.items.length; i < l; i++) {
         alltrackstoptracks.push(response.items[i]);
       }
--- src/playlistcreator/selectMood.js
+++ src/playlistcreator/selectMood.js
@@ -1,46 +1,42 @@
+import React from "react";
+import ProgressBar from "../HeaderAndFooter/header.js";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Tooltip from "rc-tooltip";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import "rc-tooltip/assets/bootstrap.css";
 
-    import React from "react";
-    import ProgressBar from '../HeaderAndFooter/header.js';
-    import { Button } from 'react-bootstrap'
-    import {Link} from 'react-router-dom';
-    import Tooltip from 'rc-tooltip';
-    import Slider from 'rc-slider';
-    import 'rc-slider/assets/index.css';
-    import 'rc-tooltip/assets/bootstrap.css';
+export default class ChooseMood extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      mood: 0.5,
+    };
+  }
 
-    export default class ChooseMood extends React.Component {
-      constructor(props) {
-        super(props);
-        this.props=props
-        this.state = {
-          mood:5  
-      };
-      }
+  handleChange = (mood) => {
+    this.setState({ mood });
+  };
 
-      handleChange = mood => {
-        this.setState({ mood });
-      };
+  componentDidMount() {
+    this.props.model.addObserver(() => this.update());
+  }
 
-    componentDidMount() {
-        this.props.model.addObserver(() => this.update());
-        
-      }
-     
-    componentWillUnmount() {
-        this.props.model.removeObserver(this)
-      }
+  componentWillUnmount() {
+    this.props.model.removeObserver(this);
+  }
 
-
-    render() {
-      
-      const {mood} = this.state;
-      const createSliderWithTooltip = Slider.createSliderWithTooltip;
-      // eslint-disable-next-line no-unused-vars
-      const Range = createSliderWithTooltip(Slider.Range);
-      const Handle = Slider.Handle;
-      const positionmarks = {0:'SAD',10:"HAPPY"};
-      const wrapperStyle = { width: 400, margin: 30 };
-      const handle = (props) => {
+  render() {
+    const { mood } = this.state;
+    const createSliderWithTooltip = Slider.createSliderWithTooltip;
+    // eslint-disable-next-line no-unused-vars
+    const Range = createSliderWithTooltip(Slider.Range);
+    const Handle = Slider.Handle;
+    const positionmarks = { 0: "SAD", 10: "HAPPY" };
+    const wrapperStyle = { width: 400, margin: 30 };
+    const handle = (props) => {
       const { value, dragging, index, ...restProps } = props;
       return (
         <Tooltip
@@ -52,31 +48,46 @@
         >
           <Handle value={value} {...restProps} />
         </Tooltip>
-      );};
+      );
+    };
 
-    return (<div className="outsideDiv">
-    <ProgressBar step={"2"}/>
-    <p className="vjueHeader"> CHOOSE PLAYLIST MOOD</p>
-    <img className = "heroPic" src={this.props.model.getHeroImage()} alt="img"></img>
-    <div style={wrapperStyle}></div>
-    <p>
-      MOOD
-      <Slider id="mood"
-        min={0} 
-        max={10} 
-        onChange={this.handleChange}
-        defaultValue={mood} 
-        marks= {positionmarks} 
-        step={0.1} 
-        handle={handle}
-      />
-    </p>
-    <div className="divider"></div>
-    <div class="text-center">
-    <Link to="/chooseEnergy"><Button variant="btn btn-success btn-lg" onClick={()=>{
-        this.props.model.setMood(mood)}} >NEXT</Button></Link>
-    </div>
-    </div>)
-    }
-
+    return (
+      <div className="outsideDiv">
+        <ProgressBar step={"2"} />
+        <p className="vjueHeader"> CHOOSE PLAYLIST MOOD</p>
+        <img
+          className="heroPic"
+          src={this.props.model.getHeroImage()}
+          alt="img"
+        ></img>
+        <div style={wrapperStyle}></div>
+        <p>
+          MOOD
+          <Slider
+            id="mood"
+            min={0}
+            max={1}
+            onChange={this.handleChange}
+            defaultValue={mood}
+            marks={positionmarks}
+            step={0.01}
+            handle={handle}
+          />
+        </p>
+        <div className="divider"></div>
+        <div class="text-center">
+          <Link to="/chooseEnergy">
+            <Button
+              variant="btn btn-success btn-lg"
+              onClick={() => {
+                this.props.model.setMood(mood);
+              }}
+            >
+              NEXT
+            </Button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
