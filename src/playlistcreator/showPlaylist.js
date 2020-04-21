fdb6f455180c878a870b9468308ed22713e42e94
Osqledarenmaster <Osqledaren@ths.kth.se>
2020-04-21 11:31:03 +0200

    hahah

--- src/playlistcreator/showPlaylist.js
+++ src/playlistcreator/showPlaylist.js
@@ -22,7 +22,10 @@ import React from "react";
             <img className = "heroPic" src={this.props.model.getHeroImage()} alt="img"></img>
             <div style={wrapperStyle} className="divider"></div>
             <p>
-                Congratulations - enjoy and share it with your friends!
+            Congratulations! This is {this.props.model.getHeroName()}'s perfect Spotify playlist. Listen to it and dream of saving the world!</p>
+            <br></br>
+            <p>Share it with your friends, or create a new one. A hero never runs out
+            of missions!
             </p>
             <div className="divider"></div>
             <div className="tablecentering">
@@ -31,10 +34,15 @@ import React from "react";
             </div>
             <div className="divider"></div>
             <div class="text-center">
+<<<<<<< HEAD
             <Link to="/chooseHero">
                 <Button variant="btn btn-success btn-lg" onClick={()=>{
                 }} > GET YOUR PLAYLIST</Button>
             </Link>
+=======
+            <Link to="/chooseHero"><Button variant="btn btn-success btn-lg" onClick={()=>{
+                }} > CREATE NEW PLAYLIST</Button></Link>
+>>>>>>> bf87e8dbd71431d343bd13b3821c1f0b4d1f7779
             </div>
             </div>
         )
