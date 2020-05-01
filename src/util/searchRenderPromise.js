93b99a286c74284052c6bf8879548d233e2fe787
majacobs <majacobs@kth.se>
2020-05-01 13:10:41 +0200

    spinner and logo

--- src/util/searchRenderPromise.js
+++ src/util/searchRenderPromise.js
@@ -1,35 +1,44 @@
-import React from 'react';
-import nothingfound from './nothingfound.gif'
-import spin from './spinner.gif'
+import React from "react";
+import nothingfound from "./nothingfound.gif";
+import spin from "./herospinner.gif";
 
 const h = React.createElement;
 
-export default function SearchRenderPromise({promise, renderData}){
+export default function SearchRenderPromise({ promise, renderData }) {
+  const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    setData(null);
+    promise
+      .then(sleeper(2000))
+      .then((x) => setData(x))
+      .catch((err) => setData({ error: err }));
+  }, [promise]); // TODO: return cancel promise on unmount
 
-    const [data, setData]=React.useState(null);
-    React.useEffect(()=>{setData(null);
-          promise.then(sleeper(2000)).then(x=>setData(x))
-                 .catch(err=>setData({error:err}));
-    }, [promise]);  // TODO: return cancel promise on unmount
- 
-    return  (data===null && h("img", {src:spin}))
-         || (data !==null && data!==undefined && !data.error && h(renderData, {data}))
-         || (data !==null && data===undefined && nothingfoundmessage());
- };
+  return (
+    (data === null && h("img", { src: spin })) ||
+    (data !== null &&
+      data !== undefined &&
+      !data.error &&
+      h(renderData, { data })) ||
+    (data !== null && data === undefined && nothingfoundmessage())
+  );
+}
 
- function sleeper(ms) {
-    return function(x) {
-      return new Promise(resolve => setTimeout(() => resolve(x), ms));
-    };
-  }
+function sleeper(ms) {
+  return function (x) {
+    return new Promise((resolve) => setTimeout(() => resolve(x), ms));
+  };
+}
 
-  function nothingfoundmessage(){
-      return h("div", {class:"Herocard"},
-      h("div", {class:"divider"},), 
-    h("img", {src:nothingfound}, null),
-    h("div", {class:"divider"},),
-    h("p",{class:"copy"}, "Nothing found on this search but me...."),
-    h("p",{class:"copy"}, "TRY AGAIN!"),
-    h("div", {class:"divider"},), 
-    )
-  }
+function nothingfoundmessage() {
+  return h(
+    "div",
+    { class: "Herocard" },
+    h("div", { class: "divider" }),
+    h("img", { src: nothingfound }, null),
+    h("div", { class: "divider" }),
+    h("p", { class: "copy" }, "Nothing found on this search but me...."),
+    h("p", { class: "copy" }, "TRY AGAIN!"),
+    h("div", { class: "divider" })
+  );
+}
