7bd3ce8189d212c5909ebae9a2cb5f5a4ec2de1b
majacobs <majacobs@kth.se>
2020-05-04 15:12:41 +0200

    städa

--- src/components/HeaderAndFooter/footer.js
+++ src/components/HeaderAndFooter/footer.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const h = React.createElement;
 
-    export default class Footer extends React.Component {
-        constructor () {
-          super();
-          
-          this.state = {};
-        }
-        render() {
-          return h("table", {className:"footer"},
-          h("tr", {}, h("th", {className:"footer"}, "LEGAL"), h("th", {className:"footer"}, "PRIVACY"),  h("th", {className:"footer"}, "COOKIES"), h("th", {className:"footer"}, "© 2020 Hero-Ify AB"), h("th", {className:"footer"}, "SWEDEN")),
-          );
-        }
-    }
+export default class Footer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+  render() {
+    return h(
+      "table",
+      { className: "footer" },
+      h(
+        "tbody",
+        {},
+        h(
+          "tr",
+          {},
+          h("th", { className: "footer" }, "LEGAL"),
+          h("th", { className: "footer" }, "PRIVACY"),
+          h("th", { className: "footer" }, "COOKIES"),
+          h("th", { className: "footer" }, "© 2020 Hero-Ify AB"),
+          h("th", { className: "footer" }, "SWEDEN")
+        )
+      )
+    );
+  }
+}
