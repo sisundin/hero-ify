8d6e2cff54d14c212c4d5d00e71b9218c873f249
Simon Sundin <simonsundinsmail@gmail.com>
2020-05-05 15:42:57 +0200

    fix

--- src/components/allPlaylistsCreated.js
+++ src/components/allPlaylistsCreated.js
@@ -34,7 +34,7 @@ export default function LatestPlaylist(props){
         return <Table class="table table-borderless" className="playlistTable">
             <thead><tr><th>User</th><th>Hero</th><th>Link</th></tr></thead>
             {data.map(playlistobject => 
-        h("tr", {} , h("td", {}, playlistobject.User), h("td", {}, playlistobject.Hero), h("td", {}, h("a", {href:"playlistobject.PlaylistLink" , target:"_blank"}, playlistobject.PlaylistLink))))
+        h("tr", {} , h("td", {}, playlistobject.User), h("td", {}, playlistobject.Hero), h("td", {}, <a href={playlistobject.PlaylistLink} target="_blank"> {playlistobject.PlaylistLink}</a>)))
         }
         </Table>
     }
