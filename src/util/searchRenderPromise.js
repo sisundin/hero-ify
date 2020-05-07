96c781fac278bb4fb586cdeb9636fddbd4990564
majacobs <majacobs@kth.se>
2020-05-07 20:35:52 +0200

    coverimage manualrequest

--- src/util/searchRenderPromise.js
+++ src/util/searchRenderPromise.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import nothingfound from './nothingfound.gif'
-import spin from '../Assets/spinner_highres.gif'
-import Back from "../components/HeaderAndFooter/Back.js"
+import React from "react";
+import nothingfound from "./nothingfound.gif";
+import spin from "../Assets/spinner_highres.gif";
+import Back from "../components/HeaderAndFooter/Back.js";
 
 const h = React.createElement;
 
@@ -14,11 +14,16 @@ export default function SearchRenderPromise({ promise, renderData }) {
       .then((x) => setData(x))
       .catch((err) => setData({ error: err }));
   }, [promise]); // TODO: return cancel promise on unmount
- 
-    return  (data===null && h("img", {className:"spinner-gif", src:spin}))
-         || (data !==null && data!==undefined && !data.error && h(renderData, {data}))
-         || (data !==null && data===undefined && nothingfoundmessage());
- };
+
+  return (
+    (data === null && h("img", { className: "spinner-gif", src: spin })) ||
+    (data !== null &&
+      data !== undefined &&
+      !data.error &&
+      h(renderData, { data })) ||
+    (data !== null && data === undefined && nothingfoundmessage())
+  );
+}
 
 function sleeper(ms) {
   return function (x) {
@@ -26,14 +31,15 @@ function sleeper(ms) {
   };
 }
 
-  function nothingfoundmessage(){
-      return h("div", {class:"divider"}, 
-    h("img", {src:nothingfound}, null),
-    h("div", {class:"divider"},),
-    h("p",{class:"copy"}, "Nothing found here but me...."),
-    h("p",{class:"copy"}, "Try something else!"),
-    h("div", {class:"divider"},),
+function nothingfoundmessage() {
+  return h(
+    "div",
+    { className: "divider" },
+    h("img", { src: nothingfound }, null),
+    h("div", { className: "divider" }),
+    h("p", { className: "copy" }, "Nothing found here but me...."),
+    h("p", { className: "copy" }, "Try something else!"),
+    h("div", { className: "divider" }),
     <Back></Back>
-    
-    )
-  }
+  );
+}
