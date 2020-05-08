1c331fcc240420cc50304d07997a5d4153119a8a
nikeer <nikeer@kth.se>
2020-05-08 12:11:56 +0200

    ändra popup

--- src/components/allPlaylistsCreated.js
+++ src/components/allPlaylistsCreated.js
@@ -15,7 +15,7 @@ export default function LatestPlaylist(props){
       
         return <div className='popup' >
             
-        <div className='popup_inner'>
+        <div className='popup_inner_playlists'>
         <div className='popup-container'>
         <h1>Playlists created by others</h1>
         <RenderPromise
