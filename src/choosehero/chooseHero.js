b40fd8843dfe2070a5c6bcfdeb47c4bfe51cadd9
Simon Sundin <simonsundinsmail@gmail.com>
2020-05-09 15:25:04 +0200

    vfvfd

--- src/choosehero/chooseHero.js
+++ src/choosehero/chooseHero.js
@@ -64,8 +64,6 @@ export default class ChooseHero extends React.Component {
                 promise={this.props.model.searchHero(this.state.freetext)}
                 renderData={({ data }) => this.createHeroDisplay(data)}
               />
-              
-            
           </div>
         </div>
       </div>
