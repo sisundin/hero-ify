61e45d1719469700b84fa2ced8caeb8f27a551f9
Simon Sundin <simonsundinsmail@gmail.com>
2020-05-09 14:34:29 +0200

    Lite css fix

--- src/components/othersplaylists/allPlaylistsCreated.js
+++ src/components/othersplaylists/allPlaylistsCreated.js
@@ -16,11 +16,16 @@ export default function LatestPlaylist(props){
         return <div className='popup' >
         <div className='popup_inner_playlists'>
         <div className='popup-container'>
+        <div className="divider"></div>
+          <div className="divider"></div>
         <h1>Playlists created by others</h1>
+        <div className="divider"></div>
         <RenderPromise
         promise = {props.model.getOthersPlaylistsfromdatabase(9)}
         renderData = { ({data}) => rendertable(data)}
         />
+        <div className="divider"></div>
+          <div className="divider"></div>
         <p className = "closeEx" onClick={props.closePlaylists}>x</p>
         </div>
         </div>
@@ -32,7 +37,7 @@ export default function LatestPlaylist(props){
         return <Table variant="table table-borderless" className="playlistTable">
             <thead><tr><th>User</th><th>Hero</th><th>Link</th></tr></thead>
             {data.map(playlistobject =>
-        h("tbody",{key:playlistobject.User}, h("tr", {} , h("td", {}, playlistobject.User), h("td", {}, playlistobject.Hero), h("td", {}, <a href={playlistobject.PlaylistLink} target="_blank" rel="noopener noreferrer"> {playlistobject.PlaylistLink}</a>))))
+        h("tbody",{key:playlistobject.User}, h("tr", {} , h("td", {}, playlistobject.User), h("td", {}, playlistobject.Hero), h("td", {}, <a  href={playlistobject.PlaylistLink} target="_blank" rel="noopener noreferrer">Link to playlist</a>))))
         }
         </Table>
     }
